0648558afdb2f5834776668066e1c0e3b5603b32
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-11-29 13:26:07 -0400

    initial commit

---
+++ Controllers/Pokemon.controller.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const createError = require('http-errors');
+const Pokemon = require('../Models/Pokemon.model');
+
+module.exports = {
+    getAllPokemon: async(req,res,next) =>{
+        
+    }
+};
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const createError = require('http-errors');
+const app = express();
+const dotenv = require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect(process.env.MONGODB_URI, {
+    dbName: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS
+}).then(() => {
+    console.log('MongoDB HAS CONNECTED....');
+});
+
+app.use((req, res, next) =>{
+    next(createError(404, "Not Found"));
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    })
+});
+
+app.listen(3000, () =>
+    {
+        console.log('server started on port 3000')
+});
